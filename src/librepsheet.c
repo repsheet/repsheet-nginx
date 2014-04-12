f7ef0a64b8177ab08f431495cb7eb6be96f1f859
Aaron Bedra <aaron@aaronbedra.com>
2014-04-12 17:42:44 -0500

    Adding doxygen documentation
    Wiring up doxygen and putting docs on the current functions
    
    Signed-off-by: Aaron Bedra <aaron@aaronbedra.com>

--- src/librepsheet.c
+++ src/librepsheet.c
@@ -1,5 +1,36 @@
+/**
+ * @file librepsheet.c
+ * @author Aaron Bedra
+ * @date 4/12/2014
+ */
+
+/*! \mainpage librepsheet
+ *
+ * \section intro_sec Introduction
+ *
+ * librepsheet is the core logic library for Repsheet. It is used in the Repsheet webserver modules as well as the backend.
+ *
+ * \section install_sec Installation
+ *
+ * librepsheet can be installed via your package manager of choice or compiled from source
+ *
+ * \subsection step1 From source
+ *
+ * TODO: write this
+ */
+
+
 #include "librepsheet.h"
 
+/**
+ * This function establishes a connection to Redis
+ *
+ * @param host the hostname of the Redis server
+ * @param port the port number of the Redis server
+ * @param timeout the length in milliseconds before the connection times out
+ *
+ * @returns a Redis connection
+ */
 redisContext *get_redis_context(char *host, int port, int timeout)
 {
   redisContext *context;
@@ -20,11 +51,24 @@ redisContext *get_redis_context(char *host, int port, int timeout)
   }
 }
 
+/**
+ * Increments the number of times a rule has been triggered for an actor
+ *
+ * @param context the Redis connection
+ * @param actor the IP address of the actor
+ * @param rule the ModSecurity rule number
+ */
 void increment_rule_count(redisContext *context, char *actor, char *rule)
 {
   freeReplyObject(redisCommand(context, "ZINCRBY %s:detected 1 %s", actor, rule));
 }
 
+/**
+ * Adds the actor to the Repsheet
+ *
+ * @param context the Redis connection
+ * @param actor the IP address of the actor
+ */
 void mark_actor(redisContext *context, char *actor)
 {
   freeReplyObject(redisCommand(context, "SET %s:repsheet true", actor));
