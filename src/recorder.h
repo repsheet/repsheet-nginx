63db0c7e7b91c15cc06ad6281523c2855a53925a
Aaron Bedra <aaron@aaronbedra.com>
2015-04-07 20:21:04 -0500

    Ditto for the recorder.

--- src/recorder.h
+++
@@ -1,8 +0,0 @@
-#ifndef __RECORDER_H
-#define __RECORDER_H
-
-int record(redisContext *context, char *timestamp, const char *user_agent,
-           const char *http_method, char *uri, char *arguments, int redis_max_length,
-           int redis_expiry, const char *actor);
-
-#endif
