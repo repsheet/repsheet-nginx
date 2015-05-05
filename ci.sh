e4e91ee8add9ab0c981dbd2ca735ade83dca9a6c
Aaron Bedra <aaron@aaronbedra.com>
2015-05-05 13:10:31 -0500

    Add ci.sh and update .travis.sh

--- ci.sh
+++ ci.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# Install a supported version of hiredis
+curl -O -L https://github.com/redis/hiredis/archive/v0.13.1.tar.gz
+tar xzf v0.13.1.tar.gz
+cd hiredis-0.13.1
+./configure
+make
+sudo make install
+sudo ldconfig
+cd ..
+
+# Install librepsheet
 git clone git://github.com/repsheet/librepsheet
 cd librepsheet
 ./autogen.sh
@@ -9,6 +20,8 @@ sudo make install
 sudo ldconfig
 pkg-config --list-all
 cd ..
+
+# Compile and run tests
 script/bootstrap
 bundle install
 bundle exec rake
