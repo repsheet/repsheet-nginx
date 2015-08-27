d786e96aa68812cb9c1e6cbd26a29ded962c7ce4
David Hanley <dhanley@groupon.com>
2015-08-27 10:27:38 -0500

    specialize vector for the time being

--- src/vector.c
+++ src/vector.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
+#include <string.h>
+
 #include "vector.h"
 
 int init_expanding_vector( struct expanding_vector *ev, int element_size, int initial_size )
 {
   ev->size = 0;
   ev->alloced_size = initial_size;
-  ev->element_size = element_size;
   ev->data = malloc( element_size * initial_size );
   return( ev->data != NULL ); 
 }
@@ -15,14 +16,14 @@ void clear_expanding_vector( struct expanding_vector *ev )
   ev->size = 0;
 }
 
-void *get_next_item( struct expanding_vector *ev ) 
+void push_item( struct expanding_vector *ev , range *push_range ) 
 {
-  ++ev->size;
-  if ( ev->size >= ev->alloced_size ) {
+  if ( ev->size == ev->alloced_size ) {
     ev->alloced_size *= 2;
-    ev->data = realloc( ev->data , ev->element_size * ev->alloced_size );
+    ev->data = realloc( ev->data , ev->alloced_size * sizeof( range ) );
   }
-  return ev->data + ( ev->size - 1 ) * ev->element_size;
+  memcpy( ev->data + ev->size , push_range , sizeof( range ) );
+  ++ev->size;
 }
   
 
--- src/vector.h
+++ src/vector.h
@@ -1,13 +1,17 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include "cidr.h"
 
 struct expanding_vector 
 {
   int size;
   int alloced_size;
-  int element_size;
-  void * data;
+  range* data;
 };
 
 int init_expanding_vector( struct expanding_vector * , int element_size , int initial_size );
 void clear_expanding_vector( struct expanding_vector *ev );
-void *get_next_item( struct expanding_vector *ev );
+void push_item( struct expanding_vector *ev , range *push_range );
 
+#endif
