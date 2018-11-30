ff0240ef17113e0a01af6a8b7f3408d0278fd0c4
Aaron Bedra <aaron@aaronbedra.com>
2018-11-30 15:28:32 -0600

    Remove travis redis from yml and install custom version

--- ci.sh
+++ ci.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+# Install most current version of redis
+git clone git://github.com/antirez/redis
+cd redis
+make
+sudo make install
+
 # Install Redis Module
 git clone git://github.com/repsheet/redis_module
 cd redis_module
 make
-redis-cli module load ./module.so
+redis-server --loadmodule ./module.so &
 cd ..
 
 # Compile and run tests
